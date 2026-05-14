b388858bfea141f3ef645f4f820c761a4f929835
Pe4enushko <mr.stone9999@gmail.com>
2026-05-14 14:42:01 +0400

    dead code

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,6 +1,5 @@
 from .docs_storage import DocsStorage
 from .done_cards_storage import DoneCardsStorage
 from .models import Doc, Result
-from .results_storage import ResultsStorage
 
-__all__ = ["DocsStorage", "DoneCardsStorage", "ResultsStorage", "Doc", "Result"]
+__all__ = ["DocsStorage", "DoneCardsStorage", "Doc", "Result"]
