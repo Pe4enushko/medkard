a5f7d719232bbb5cf469a7db8a6c87c885a30670
Pe4enushko <mr.stone9999@gmail.com>
2026-05-25 13:50:20 +0400

    higher

--- parsers/excel.py
+++ parsers/excel.py
@@ -161,7 +161,7 @@ class AuditExcelWriter:
             wb, ws = self._open_or_create()
             ws.append(row)
             new_row = ws.max_row
-            ws.row_dimensions[new_row].height = 250
+            ws.row_dimensions[new_row].height = 500
             for col_letter in _WRAPPED_COLUMNS:
                 col_idx = openpyxl.utils.column_index_from_string(col_letter)
                 ws.cell(row=new_row, column=col_idx).alignment = Alignment(wrap_text=True, vertical="top")
